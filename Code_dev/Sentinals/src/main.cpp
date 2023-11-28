3b16fa451ad3e8b5cf7c5b6abb161ce413bb6436
neocodered <nadilhansith@gmail.com>
2023-11-28 14:09:49 +0530

    calibrating function implementation

--- Code_dev/Sentinals/src/main.cpp
+++ Code_dev/Sentinals/src/main.cpp
@@ -16,25 +16,32 @@ void portFix();
 void rightmotor(float speed);
 void leftmotor(float speed);
 float line();
+void calibrate();
+
+//Sensor calibration data store arrays
+int sensMax[8] = {0,0,0,0,0,0,0,0};
+int sensMin[8] = {0,0,0,0,0,0,0,0};
 
 void setup() {
   Serial.begin(9600);
   portFix();
+  calibrate();
 }
 
 void loop() {
 
-  int Kp = 4;
-  int baseSpeed = 60;
+  // int Kp = 4;
+  // int baseSpeed = 60;
 
   float error = line();
+  Serial.println(error);
 
-  float p = error*Kp;
+  // float p = error*Kp;
 
-  float pid = Kp*p;
+  // float pid = Kp*p;
 
-  rightmotor(baseSpeed - pid);
-  leftmotor(baseSpeed + pid);
+  // rightmotor(baseSpeed - pid);
+  // leftmotor(baseSpeed + pid);
 
 }
 
@@ -64,15 +71,15 @@ void rightmotor(float speed){
   if(speed>0){
     //Forward rotation
     digitalWrite(INB_1,LOW);
-    //Setting speed
     speed = speed;
   }
   else if(speed<0){
     //Backward rotation
     digitalWrite(INB_1,HIGH);
-    //Setting speed
     speed = -speed;
   }
+  //Setting speed
+  analogWrite(INA_1,speed);
   
 }
 
@@ -82,39 +89,55 @@ void leftmotor(float speed){
   if(speed>0){
     //Forward rotation
     digitalWrite(INB_2,HIGH);
-    //Setting speed
-    analogWrite(INA_2,speed);
+    speed = speed;
   }
   else if(speed<0){
     //Backward rotation
     digitalWrite(INB_2,LOW);
-    //Setting speed
-    analogWrite(INA_2,-speed);
+    speed = -speed;
   }
+  //Setting speed
+  analogWrite(INA_2,speed);
   
 }
 
 float line(){
-  int threshold = 50; //White-black analog threshold value
   int sensVals[8]; // List of sensory data
   int k = 0;
+  int weighted_total = 0;
   int total = 0;
-  int elNum = 0;
 
   for (int j = A0; j <= A7; ++j) {
-    sensVals[k] = (threshold < analogRead(j))*k;
+    sensVals[k] = analogRead(j);
     k++;
   }
 
   for (int k=0; k<=7; k++){
+    sensVals[k] = map(sensVals[k],sensMin[k],sensMax[k],0,1);
+    weighted_total += sensVals[k] * k;
     total += sensVals[k];
-    elNum += sensVals[k] > 0;
   }
 
-  if (elNum == 0){
-    elNum = 1;
+  if (total == 0){
+    total = 1;
   }
 
-  return total/elNum - 3.5;
+  return weighted_total/total - 3.5;
 }
 
+void calibrate(){
+  int l = 0;
+
+  for (int l=0; l<=99; l++)
+    for (int j = A0; j <= A7; ++j) {
+      if (sensMax[l] < analogRead(j)){
+        sensMax[l] = analogRead(j);
+      }
+      if (sensMin[l] > analogRead(j)){
+        sensMin[l] = analogRead(j);
+      }
+      l++;
+    }
+    delay(50);
+
+}
