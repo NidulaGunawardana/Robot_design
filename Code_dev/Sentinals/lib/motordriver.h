6993bc65bd81230606215db101e338f52ba83a91
neocodered <nadilhansith@gmail.com>
2023-11-29 13:33:53 +0530

    driver_test

--- Code_dev/Sentinals/lib/motordriver.h
+++ Code_dev/Sentinals/lib/motordriver.h
@@ -4,5 +4,5 @@
 void pinSetup(int INA_1, int INB_1, int EN_1, int ENCA_1, int ENCB_1);
 void readEncoder();
 float getSpeed();
-void setSpeed(int speedToSet,int motor,int dir,int side)
+void setSpeed(int speedToSet,int motor,int dir,int side);
 #endif
