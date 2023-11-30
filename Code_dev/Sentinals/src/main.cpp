1bdbc84dc15f7e45d60c4e0816dc9159acd50bc8
neocodered <nadilhansith@gmail.com>
2023-12-01 01:08:02 +0530

    Line follow mid eval code

--- Code_dev/Sentinals/src/main.cpp
+++ Code_dev/Sentinals/src/main.cpp
@@ -26,6 +26,7 @@ float speedSet1;
 
 // function declrarions
 
+<<<<<<< HEAD
 // Sensor calibration data store arrays
 int sensMax[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 int sensMin[8] = {100, 100, 100, 100, 100, 100, 100, 100};
@@ -56,6 +57,33 @@ void readEncoder2()
   {
     pos2--;
   }
+=======
+//Sensor calibration data store arrays
+int sensMax[8] = {0,0,0,0,0,0,0,0};
+int sensMin[8] = {0,0,0,0,0,0,0,0};
+
+void setup() {
+  Serial.begin(9600);
+  portFix();
+  calibrate();
+}
+
+void loop() {
+
+  // int Kp = 4;
+  // int baseSpeed = 60;
+
+  float error = line();
+  Serial.println(error);
+
+  // float p = error*Kp;
+
+  // float pid = Kp*p;
+
+  // rightmotor(baseSpeed - pid);
+  // leftmotor(baseSpeed + pid);
+
+>>>>>>> parent of 3fbea41 (end day 2 not workig : ()
 }
 
 float getSpeed1()
@@ -80,6 +108,7 @@ float getSpeed2()
     prev_pos2 = pos2;
     delay(10);
   }
+<<<<<<< HEAD
   moving_avg2 = map(moving_avg2, 0, 100, 0, 255);
   return moving_avg2;
 }
@@ -138,4 +167,96 @@ void loop()
   setSpeed1(speedRRead, INA_1, INB_1, HIGH);
   setSpeed2(speedRRead, INA_2, INB_2, HIGH);
   // Serial.println(speedRRead);
+=======
+  else if(speed<0){
+    //Backward rotation
+    digitalWrite(INB_1,HIGH);
+    speed = -speed;
+  }
+  //Setting speed
+  speedSet = speed;
+  while (encSpeedright < speed - err_margin || encSpeedright > speed + err_margin){
+    if (encSpeedright < speed){
+      speedSet++;
+    }
+    else{
+      speedSet--;
+    }
+    analogWrite(INA_1,speedSet);
+  }
+  
+  
+}
+
+// Left motor working 
+void leftmotor(float speed){
+  int err_margin = 5;
+  int speedSet;
+  digitalWrite(EN2,HIGH);
+  if(speed>0){
+    //Forward rotation
+    digitalWrite(INB_2,HIGH);
+    speed = speed;
+  }
+  else if(speed<0){
+    //Backward rotation
+    digitalWrite(INB_2,LOW);
+    speed = -speed;
+  }
+  //Setting speed
+  speedSet = speed;
+  while (encSpeedleft < speed - err_margin || encSpeedleft > speed + err_margin){
+    if (encSpeedleft < speed){
+      speedSet++;
+    }
+    else{
+      speedSet--;
+    }
+    analogWrite(INA_2,speedSet);
+  }
+  
+}
+
+//Calculating the error of line following
+float line(){
+  int sensVals[8]; // List of sensory data
+  int k = 0;
+  int weighted_total = 0;
+  int total = 0;
+
+  for (int j = A0; j <= A7; ++j) {
+    sensVals[k] = analogRead(j);
+    k++;
+  }
+
+  for (int k=0; k<=7; k++){
+    sensVals[k] = map(sensVals[k],sensMin[k],sensMax[k],0,1);
+    weighted_total += sensVals[k] * k;
+    total += sensVals[k];
+  }
+
+  if (total == 0){
+    total = 1;
+  }
+
+  return weighted_total/total - 3.5;
+}
+
+//Calibrating the line following sensors for 5 seconds
+void calibrate(){
+  int l = 0;
+
+  for (int l=0; l<=99; l++)
+    for (int j = A0; j <= A7; ++j) {
+      if (sensMax[l] < analogRead(j)){
+        sensMax[l] = analogRead(j);
+      }
+      if (sensMin[l] > analogRead(j)){
+        sensMin[l] = analogRead(j);
+      }
+      l++;
+    }
+    delay(50);
+
+>>>>>>> parent of 3fbea41 (end day 2 not workig : ()
 }
