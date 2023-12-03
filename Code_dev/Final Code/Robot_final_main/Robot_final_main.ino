f027e212c26a80710c1d907793a461979a1c923a
Nidula Gunawardana <94860959+nidulaguna@users.noreply.github.com>
2023-12-03 20:27:08 +0530

    line following added to main file

--- Code_dev/Final Code/Robot_final_main/Robot_final_main.ino
+++ Code_dev/Final Code/Robot_final_main/Robot_final_main.ino
@@ -1,5 +1,6 @@
 #include <EasyNextionLibrary.h>
 #include "distance.h"
+#include "line_following.h"
 EasyNex disp = EasyNex(Serial);
 
 float speed = 62;
@@ -7,11 +8,13 @@ int level = 1;
 float kp = 0.041;
 float kd = 0.1;
 bool measureDistance = false;
+bool lineFollowing = false;
 
 
 void setup() {
   disp.begin(9600);
   setID();
+  portFix();
 }
 
 void loop() {
@@ -19,17 +22,42 @@ void loop() {
   if (measureDistance == true) {
     disp.writeStr("t0.txt", (String)sensor_1());
   }
+  if (lineFollowing == true) {
+    linefollow();
+  }
 }
 
 
 //Start button
 void trigger0() {
-  if (level == 1 || level == 3 || level == 4 || level == 5 || level == 6) {
+  if (level == 1) {
     disp.writeStr("page task1");
     measureDistance = false;
-  } else if (level == 2 || level == 7) {
+    lineFollowing = true;
+  } else if (level == 2) {
     disp.writeStr("page task2");
     measureDistance = true;
+    lineFollowing = false;
+  } else if (level == 3) {
+    disp.writeStr("page task1");
+    measureDistance = false;
+    lineFollowing = false;
+  } else if (level == 4) {
+    disp.writeStr("page task1");
+    measureDistance = false;
+    lineFollowing = false;
+  } else if (level == 5) {
+    disp.writeStr("page task1");
+    measureDistance = false;
+    lineFollowing = false;
+  } else if (level == 6) {
+    disp.writeStr("page task1");
+    measureDistance = false;
+    lineFollowing = false;
+  } else if (level == 7) {
+    disp.writeStr("page task2");
+    measureDistance = false;
+    lineFollowing = false;
   }
 }
 
@@ -37,6 +65,7 @@ void trigger0() {
 void trigger1() {
   level--;
   measureDistance = false;
+  lineFollowing = false;
   disp.writeStr("t0.txt", (String)level);
 }
 
@@ -44,6 +73,7 @@ void trigger1() {
 void trigger2() {
   level++;
   measureDistance = false;
+  lineFollowing = false;
   disp.writeStr("t0.txt", (String)level);
 }
 
@@ -51,6 +81,7 @@ void trigger2() {
 void trigger3() {
   speed--;
   measureDistance = false;
+  lineFollowing = false;
   disp.writeStr("t2.txt", (String)speed);
 }
 
@@ -58,18 +89,23 @@ void trigger3() {
 void trigger4() {
   speed++;
   measureDistance = false;
+  lineFollowing = false;
   disp.writeStr("t2.txt", (String)speed);
 }
 
 // calibrate button
 void trigger5() {
   measureDistance = false;
+  lineFollowing = false;
+  calibrate();
+  delay(7000);
 }
 
 // Home button
 void trigger6() {
   speed++;
   measureDistance = false;
+  lineFollowing = false;
   disp.writeStr("page home");
   disp.writeStr("t0.txt", (String)level);
   disp.writeStr("t2.txt", (String)speed);
@@ -84,6 +120,7 @@ void trigger7() {
 // Settings button
 void trigger8() {
   measureDistance = false;
+  lineFollowing = false;
   disp.writeStr("page settings");
   disp.writeStr("t3.txt", (String)kd);
   disp.writeStr("t2.txt", (String)kp);
@@ -93,6 +130,7 @@ void trigger8() {
 void trigger9() {
   kp += 0.001;
   measureDistance = false;
+  lineFollowing = false;
   disp.writeStr("t2.txt", (String)kp);
 }
 
@@ -100,6 +138,7 @@ void trigger9() {
 void trigger10() {
   kp -= 0.001;
   measureDistance = false;
+  lineFollowing = false;
   disp.writeStr("t2.txt", (String)kp);
 }
 
@@ -107,6 +146,7 @@ void trigger10() {
 void trigger11() {
   kd += 0.01;
   measureDistance = false;
+  lineFollowing = false;
   disp.writeStr("t3.txt", (String)kd);
 }
 
@@ -115,10 +155,12 @@ void trigger11() {
 void trigger12() {
   kd -= 0.01;
   measureDistance = false;
+  lineFollowing = false;
   disp.writeStr("t3.txt", (String)kd);
 }
 
 // Task stop button
 void trigger13() {
-measureDistance = false;
+  measureDistance = false;
+  lineFollowing = false;
 }
