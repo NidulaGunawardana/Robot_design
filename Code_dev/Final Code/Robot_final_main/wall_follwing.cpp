7c5d2b7db78be2935a503edd315ae1e29070f99f
neocodered <nadilhansith@gmail.com>
2023-12-03 21:37:46 +0530

    wall follwing library base code

--- Code_dev/Final Code/Robot_final_main/wall_follwing.cpp
+++ Code_dev/Final Code/Robot_final_main/wall_follwing.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 #include "line_following.h"
+#include "distance.h"
 
 void wall_follow(float diatance){
+  int baseSpeed = 75;
   float Kp = 1;
-  
-  leftTurn()
-
+  float error = sensor_1() - distance;
+  float pid = Kp*error;
+  leftmotor(baseSpeed+pid);
+  rightmotor(baseSpeed+20-pid);
 }
